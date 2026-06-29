d6934950c152ea240979b9359b1e10761c7bfe64
Rishith Kumar <rishith.kumar@godrejagrovet.com>
2026-06-29 15:33:57 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name = " GAVL-KeyVault-State-RG"
     storage_account_name = "gavlkvstate"
-    container_name = "CONTAINER NAME"
+    container_name = "gavlterraformstatefile"
     key = "<APP>-kv.tfstate"
   }
 }
